4b643bea5a407a6bcad27dedc872fac2a0ec9dbe
Johan Talboom <borfje@gmail.com>
2019-04-05 22:49:05 +0200

    Added average usage

--- mqtt_net/SensorCloud/services/P1Meter/Service.cs
+++ mqtt_net/SensorCloud/services/P1Meter/Service.cs
@@ -7,10 +7,25 @@ using System.Threading.Tasks;
 
 namespace SensorCloud.services.P1Meter
 {
+    class Measurement
+    {
+        public DateTime time;
+        public DataPacket data;
+    }
+
     public partial class Service : SensorCloud.Service
     {
         private Config config;
         private SmartMeter meter;
+
+        List<Measurement> measurements = new List<Measurement>();
+
+        public Decimal powerOneMinute { get; private set; }
+        public Decimal powerTenMinute { get; private set; }
+
+        public Decimal gasOneMinute { get; private set; }
+        public Decimal gasTenMinute { get; private set; }
+
 
         public Service(IServiceProvider services, Config config) : base(services)
         {
@@ -27,11 +42,30 @@ namespace SensorCloud.services.P1Meter
 
         private async void OnData(object sender, DataPacket data)
         {
-            if(mqtt != null)
+            measurements.Add(new Measurement() { data = data, time = DateTime.Now });
+            while (measurements.Count > 0 && measurements[0].time.AddMinutes(10) < DateTime.Now)
+                measurements.RemoveAt(0);
+
+            DataPacket minuteOne = measurements.AsEnumerable().Reverse().TakeWhile(m => m.time.AddMinutes(1) < DateTime.Now).Last().data;
+            DataPacket minuteTen = measurements.AsEnumerable().Reverse().TakeWhile(m => m.time.AddMinutes(10) < DateTime.Now).Last().data;
+
+            powerOneMinute = (data.PowerConsumptionTariff1 - minuteOne.PowerConsumptionTariff1) + (data.PowerConsumptionTariff2 - minuteOne.PowerConsumptionTariff2);
+            powerTenMinute = (data.PowerConsumptionTariff1 - minuteTen.PowerConsumptionTariff1) + (data.PowerConsumptionTariff2 - minuteTen.PowerConsumptionTariff2);
+
+            gasOneMinute = data.GasUsage - minuteOne.GasUsage;
+            gasTenMinute = data.GasUsage - minuteTen.GasUsage;
+
+            if (mqtt != null)
             {
                 await mqtt.Publish("p1/power1", data.PowerConsumptionTariff1 + "");
                 await mqtt.Publish("p1/power2", data.PowerConsumptionTariff2 + "");
                 await mqtt.Publish("p1/gas", data.GasUsage + "");
+
+                await mqtt.Publish("p1/use/1/power", powerOneMinute + "");
+                await mqtt.Publish("p1/use/10/power", powerTenMinute + "");
+                await mqtt.Publish("p1/use/1/gas", gasOneMinute + "");
+                await mqtt.Publish("p1/use/10/gas", gasTenMinute + "");
+
             }
         }
     }
